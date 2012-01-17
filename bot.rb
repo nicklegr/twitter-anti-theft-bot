4f4d8712ea50a99ac2630f0f42118efb60d8ddde
Nickle <nickle-github@nickle.ath.cx>
2012-01-17 22:03:15 +0900

    リツイートを実装

---
+++ bot.rb
@@ -0,0 +1,24 @@
+#!ruby -Ku
+
+class Bot
+  def initialize(settings)
+    @target = settings['target']
+    @consumer_key = settings['consumer_key']
+    @consumer_secret = settings['consumer_secret']
+    @oauth_token = settings['oauth_token']
+    @oauth_token_secret = settings['oauth_token_secret']
+  end
+
+  # 同じツイートを2度リツイートすると、
+  # Twitter::Error::Forbidden(sharing is not permissable for this status (Share validations failed)) が飛ぶ
+  def retweet(id)
+    Twitter.configure do |config|
+      config.consumer_key = @consumer_key
+      config.consumer_secret = @consumer_secret
+      config.oauth_token = @oauth_token
+      config.oauth_token_secret = @oauth_token_secret
+    end
+
+    Twitter.retweet(id)
+  end
+end
--- main.rb
+++ main.rb
@@ -2,8 +2,11 @@
 
 require 'pp'
 require 'twitter'
+require 'yaml'
+
 require './search'
 require './tweet'
+require './bot'
 
 # pp Twitter.search("ふぁぼれよ from:patatoma -RT -QT", :lang => "ja", :locale => "ja", :rpp => 100, :page => 1) # , 
 
@@ -14,5 +17,8 @@ ids = Search.new.find_ids(tweet, "patatoma")
 
 original_id = Tweet.new.estimate_original(tweet, ids)
 # pp original_id
+
+bot_settings = YAML.load_file("bots.yaml")
+Bot.new(bot_settings['bots'].first).retweet(original_id)
 
 # ☆1000ふぁぼツイート★は、ときどきアフィ発言が混ざる。ユーザー名が入っていないものは除外
